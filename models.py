2ea64171a130651e4fd6db9954f86f66d0f99c67
braingilbert <briangilbert@DESKTOP-BOD1Q8N>
2025-09-04 13:13:57 +0300

    Update

--- models.py
+++ models.py
@@ -1,4 +1,4 @@
-from sqlalchemy import create_engine, Column, Integer, String, Text, Enum
+from sqlalchemy import create_engine, Column, Integer, String, Text, Enum, Boolean
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 import os
